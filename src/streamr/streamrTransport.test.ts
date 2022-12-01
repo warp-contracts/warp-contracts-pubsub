04a2511a465acadec10a82b50f47858462ec8b1a
Max Levitskiy <max.dstu@gmail.com>
2022-12-01 13:16:15 +0100

    Streamr support

---
+++ src/streamr/streamrTransport.test.ts
@@ -0,0 +1,39 @@
+import { StreamrConnectionOptions, StreamrTransport } from './streamrTransport';
+import { beforeEach, afterEach } from '@jest/globals';
+import * as dotenv from 'dotenv';
+
+describe('streamrTransport test', () => {
+  dotenv.config();
+  let streamrTransport: StreamrTransport;
+  const streamrApiKey = process.env.STREAMR_WRITE_APIKEY;
+  expect(streamrApiKey).not.toBe('');
+  const streamrHost = 'ws://redstone-nlb-prod-b3c531f79942790e.elb.eu-central-1.amazonaws.com';
+  const streamrReadPort = 7170;
+  const streamrWritePort = 7180;
+  expect(streamrHost).not.toBe('');
+  const streamId = '0xc2ae2d5523080b64cc788cddc91ff59a3e29f911/common';
+  const connection: StreamrConnectionOptions = {
+    apiKey: streamrApiKey,
+    writeHost: streamrHost,
+    writePort: streamrWritePort,
+    readHost: streamrHost,
+    readPort: streamrReadPort,
+    streamId: streamId,
+    direction: 'all'
+  };
+  describe('happy path', () => {
+    beforeEach(() => {
+      streamrTransport = new StreamrTransport(connection);
+    });
+    afterEach(() => streamrTransport.close());
+    it('should be able to publish and read message', async () => {
+      const onMessage = new Promise((resolve, reject) => {
+        streamrTransport.sub(resolve, reject);
+      });
+      const data = { some: 'object' };
+      await streamrTransport.pub(data);
+      const actual = await onMessage;
+      expect(actual).toStrictEqual(data);
+    });
+  });
+});
