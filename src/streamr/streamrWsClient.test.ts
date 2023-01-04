0b0bcc69660f7994d7feb2af0c5748f47d41ee3d
Max Levitskiy <max.dstu@gmail.com>
2023-01-04 15:58:48 +0100

    Create factory method instead of constructor for StreamrWsClient. Now, there is no way to get not connected instance of StreamrWsClient.

--- src/streamr/streamrWsClient.test.ts
+++ src/streamr/streamrWsClient.test.ts
@@ -7,33 +7,27 @@ describe('streamrTransport test', () => {
   let streamrTransport: StreamrWsClient;
   const streamrApiKey = process.env.STREAMR_WRITE_APIKEY;
   expect(streamrApiKey).not.toBe('');
-  const streamrHost = 'ws://redstone-nlb-prod-b3c531f79942790e.elb.eu-central-1.amazonaws.com';
-  const streamrReadPort = 7170;
-  const streamrWritePort = 7180;
-  expect(streamrHost).not.toBe('');
   const streamId = '0xc2ae2d5523080b64cc788cddc91ff59a3e29f911/common';
   const connection: StreamrConnectionOptions = {
     apiKey: streamrApiKey,
-    writeHost: streamrHost,
-    writePort: streamrWritePort,
-    readHost: streamrHost,
-    readPort: streamrReadPort,
     streamId: streamId,
     direction: 'all'
   };
   describe('happy path', () => {
-    beforeEach(() => {
-      streamrTransport = new StreamrWsClient(connection);
+    beforeEach(async () => {
+      streamrTransport = await StreamrWsClient.create(connection);
     });
     afterEach(() => streamrTransport.close());
-    it('should be able to publish and read message', async () => {
-      const onMessage = new Promise((resolve, reject) => {
-        streamrTransport.sub(resolve, reject);
-      });
+    it('should be able to publish and read message', (done) => {
       const data = { some: 'object' };
-      await streamrTransport.pub(data);
-      const actual = await onMessage;
-      expect(actual).toStrictEqual(data);
+      streamrTransport.sub(
+        (msg) => {
+          expect(msg).toStrictEqual(data);
+          done();
+        },
+        (err) => fail(err)
+      );
+      streamrTransport.pub(data);
     });
   });
 });
